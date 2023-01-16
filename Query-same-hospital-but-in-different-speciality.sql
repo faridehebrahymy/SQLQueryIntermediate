748fd8b87c0bd09a96c20a9f29742cdf2316ff15
faridehebrahymy <96016472+faridehebrahymy@users.noreply.github.com>
2023-01-16 14:57:01 +0330

    Add files via upload

---
+++ Query-same-hospital-but-in-different-speciality.sql
@@ -0,0 +1,51 @@
+--From the doctors table, fetch the details of doctors who work in the same hospital but in different speciality.
+
+--Table Structure:
+
+drop table doctors;
+create table doctors
+(
+id int primary key,
+name varchar(50) not null,
+speciality varchar(100),
+hospital varchar(50),
+city varchar(50),
+consultation_fee int
+);
+
+insert into doctors values
+(1, 'Dr. Shashank', 'Ayurveda', 'Apollo Hospital', 'Bangalore', 2500),
+(2, 'Dr. Abdul', 'Homeopathy', 'Fortis Hospital', 'Bangalore', 2000),
+(3, 'Dr. Shwetha', 'Homeopathy', 'KMC Hospital', 'Manipal', 1000),
+(4, 'Dr. Murphy', 'Dermatology', 'KMC Hospital', 'Manipal', 1500),
+(5, 'Dr. Farhana', 'Physician', 'Gleneagles Hospital', 'Bangalore', 1700),
+(6, 'Dr. Maryam', 'Physician', 'Gleneagles Hospital', 'Bangalore', 1500);
+select * from doctors;
+
+--Solution:
+
+select dr1.*
+from doctors dr1
+JOIN doctors dr2 
+    ON dr1.id <> dr2.id AND dr1.hospital = dr2.hospital AND dr1.speciality <> dr2.speciality
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
